f21638b5cc81dcf17302f2317a716e2bcc81f1a4
Ruam Marcos <ruam.marcos@mail.uft.edu.br>
2024-11-12 07:40:09 -0300

    v.1 estavel

--- grafico.py
+++ grafico.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-#RECEBER: pts
+f = lambda x: 2*x + 1
+
+t = np.linspace( 0, 10, 100)
+y = list(map(f, t))
+
+plt.plot(t, y)
+plt.title("Grafiquinho")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.show()
+
+
